e8a1e10e0a441067383749b22c07ff724c6ebb79
Martinek <martinmatostano@gmail.com>
2021-09-23 08:48:30 +0200

    add method of calculation if recipients will get equal slices of pie

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,27 @@
 package com.appslab;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("Total slices of pie: ");
+        int totalNumberOfSlices = console.nextInt();
+
+        System.out.print("Number Of Recipients: ");
+        int numberOfRecipients = console.nextInt();
+
+        System.out.print("How many slices each person gets: ");
+        int numberOfSlicesEachPersonGets = console.nextInt();
+
+        System.out.print("Result's: " + EqualSlices(totalNumberOfSlices, numberOfRecipients, numberOfSlicesEachPersonGets));
+    }
+
+    public static boolean EqualSlices(int totalNumberOfSlices, int numberOfRecipients, int numberOfSlicesEachPersonGets) {
+        int helper = numberOfRecipients * numberOfSlicesEachPersonGets;
+
+        return helper <= totalNumberOfSlices;
     }
 }
