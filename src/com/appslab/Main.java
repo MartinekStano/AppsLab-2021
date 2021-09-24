58419e03cad19e08a95bdaf72ff5f7333342c593
Martin Staňo <martinmatostano@gmail.com>
2021-09-24 17:33:12 +0200

    add simulation of rock, paper, scissors

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -5,22 +5,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner console = new Scanner(System.in);
 
-        System.out.print("Select Side \"A\": ");
-        int number1 = console.nextInt();
-        System.out.print("Select Side \"B\": ");
-        int number2 = console.nextInt();
-        System.out.print("Select Side \"C\": ");
-        int number3 = console.nextInt();
+        System.out.print("PLAYER 1: Rock, paper or scissors?: ");
+        String player1 = console.nextLine();
 
-        System.out.print("Its that triangle: "+IsTriangle(number1, number2, number3));
+        System.out.print("PLAYER 2: Rock, paper or scissors?: ");
+        String player2 = console.nextLine();
+
+
+        System.out.print("Result's: " + RockPaperScissors(player1, player2));
     }
-    public static boolean IsTriangle(int number1, int number2, int number3) {
-
-      int sideAB = (number1*number1) + (number2*number2) ;
-      int sideC = number3*number3;
-
-      return sideAB == sideC;
+    public static String RockPaperScissors(String player1, String player2)
+    {
+        if(player1.equals("rock") && player2.equals("rock") || player1.equals("paper") && player2.equals("paper") ||player1.equals("scissors") && player2.equals("scissors") )
+        {
+            return "TIE";
+        }
+        if(player1.equals("rock") && player2.equals("scissors") || player1.equals("scissors") && player2.equals("paper") || player1.equals("paper") && player2.equals("rock") )
+        {
+            return "PLAYER 1 WINS";
+        }
+        else
+        {
+            return "PLAYER 2 WINS";
+        }
     }
+
 }
